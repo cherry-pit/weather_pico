29ed2561750728f4e69be43042df9b08b5dda831
Cherry <cmallin54@gmail.com~>
2025-11-17 10:29:03 -0600

    Updated params

--- parameters.py
+++ parameters.py
@@ -1,13 +1,19 @@
 # Enter wifi information here
-WIFI_SSID = "XYZ"
-WIFI_PASSWORD = "ABC"
+WIFI_SSID = "SSID"
+WIFI_PASSWORD = "PASSWORD"
+
 ### Change latitude and longitude information to where weather should be reported for
-#
-lat, long =  XX.XXXX, XX.XXXX
-timezone_offset = X # hour offset from GMT timezone | for example -6 for CST
+### Use the following link to get the below varibles, update lat and long https://api.weather.gov/points/LAT,LONG
+forecastOffice = ?
+gridX = ?
+gridY = ?
+
+# Timezone offset from UTC, -6 for example for CST
+local_timezone_offset = 
 
 # the below is optional, if you fill it out a caution symbol will show up on the display if there is a weather warning, for example a wind chill warning
 # Use the folling link to find your country code https://alerts.weather.gov/
 # For example Jo Daviess county in Illinois: ILC085
 # if you dont want to use the weather warnings just leave the below as "" -- an empty string
 county_code = ""
+
